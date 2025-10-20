1439f910dec9f1e8c8596b4f866b9cb99573f730
shuflovic <pavel.skuta@gmail.com>
2025-10-20 16:39:48 +1300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'jd66-cache-v4'; // Bumped version to clear old cache
+const CACHE_NAME = 'jd66-cache-v5';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-  // Vite generates hashed assets, so we can't pre-cache them by name.
-  // The SW will cache them on the fly as they are requested.
+  '/JD_66/',
+  '/JD_66/index.html',
+  '/JD_66/manifest.json',
+  '/JD_66/icons/icon-192.png',
+  '/JD_66/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -21,28 +21,23 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-
         const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          networkResponse => {
-            if (!networkResponse || networkResponse.status !== 200 || (networkResponse.type !== 'basic' && networkResponse.type !== 'cors')) {
-              return networkResponse;
-            }
-            const responseToCache = networkResponse.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                if (event.request.method === 'GET') {
-                    cache.put(event.request, responseToCache);
-                }
-              });
+        return fetch(fetchRequest).then(networkResponse => {
+          if (!networkResponse || networkResponse.status !== 200 || (networkResponse.type !== 'basic' && networkResponse.type !== 'cors')) {
             return networkResponse;
           }
-        );
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              if (event.request.method === 'GET') {
+                cache.put(event.request, responseToCache);
+              }
+            });
+          return networkResponse;
+        });
       })
   );
 });
