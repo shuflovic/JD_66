4fb79fa2d89211dc253984372b21fbec6c34cd8d
shuflovic <pavel.skuta@gmail.com>
2025-10-22 20:02:11 +1300

    Update PlayerHand.tsx

--- src/components/PlayerHand.tsx
+++ src/components/PlayerHand.tsx
@@ -35,7 +35,7 @@ export const PlayerHand: React.FC<PlayerHandProps> = ({
 
   return (
     <div className="p-4 shadow-xl">
-      <div className="text-amber-400 text-sm font-serif uppercase tracking-widest mb-3">
+      <div className="text-amber-300 text-center text-sm font-serif uppercase tracking-widest mb-3">
         Your Hand
       </div>
       <div className={`grid gap-2 grid-cols-4`}>
