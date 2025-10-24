98d1389bac0be15d8ccef8852afe5b46b3bdcbac
shuflovic <pavel.skuta@gmail.com>
2025-10-25 08:02:55 +1300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,8 @@ const App: React.FC = () => {
                   <div className="text-sm  text-gray-500 dark:text-gray-400">Deck</div>
                   <div className="text-2xl font-bold text-gray-700 dark:text-gray-200">{deck.length}</div>
               </div>
-            
+            </div>
+          
             <button
               onClick={handleUndo}
               disabled={history.length === 0 || gameState !== GameState.PLAYING}
@@ -104,7 +105,7 @@ const App: React.FC = () => {
                     <path fillRule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
                 </svg>
             </button>
-          </div>
+          
          </div>
       </header>
       
