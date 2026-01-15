0392bffede87ee5137faad2ae79f22f446cc6978
shuflovic <pavel.skuta@gmail.com>
2026-01-15 16:24:40 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
 export default defineConfig({
-  base: '/',           // ← most important change
+  base: '/JD_66/',           // ← put it back!
   plugins: [react()],
 })
